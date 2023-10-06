2d0f6f294a0f419fece1402507fc6603f108ae96
Jaco Kok <kokjaco2@gmail.com>
2023-10-06 09:56:04 +0200

    Tailwind config to ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,9 @@
+import type { Config } from "tailwindcss";
 import { fontFamily } from "tailwindcss/defaultTheme";
+import { kayordPlugin } from "./src/lib/tailwind/plugin";
+import forms from "@tailwindcss/forms";
 
-/** @type {import('tailwindcss').Config} */
-const config = {
+export default {
 	darkMode: ["class"],
 	content: ["./src/**/*.{html,js,svelte,ts}"],
 	theme: {
@@ -9,8 +11,8 @@ const config = {
 			center: true,
 			padding: "2rem",
 			screens: {
-				"2xl": "1400px"
-			}
+				"2xl": "1400px",
+			},
 		},
 		extend: {
 			colors: {
@@ -21,43 +23,42 @@ const config = {
 				foreground: "hsl(var(--foreground))",
 				primary: {
 					DEFAULT: "hsl(var(--primary))",
-					foreground: "hsl(var(--primary-foreground))"
+					foreground: "hsl(var(--primary-foreground))",
 				},
 				secondary: {
 					DEFAULT: "hsl(var(--secondary))",
-					foreground: "hsl(var(--secondary-foreground))"
+					foreground: "hsl(var(--secondary-foreground))",
 				},
 				destructive: {
 					DEFAULT: "hsl(var(--destructive))",
-					foreground: "hsl(var(--destructive-foreground))"
+					foreground: "hsl(var(--destructive-foreground))",
 				},
 				muted: {
 					DEFAULT: "hsl(var(--muted))",
-					foreground: "hsl(var(--muted-foreground))"
+					foreground: "hsl(var(--muted-foreground))",
 				},
 				accent: {
 					DEFAULT: "hsl(var(--accent))",
-					foreground: "hsl(var(--accent-foreground))"
+					foreground: "hsl(var(--accent-foreground))",
 				},
 				popover: {
 					DEFAULT: "hsl(var(--popover))",
-					foreground: "hsl(var(--popover-foreground))"
+					foreground: "hsl(var(--popover-foreground))",
 				},
 				card: {
 					DEFAULT: "hsl(var(--card))",
-					foreground: "hsl(var(--card-foreground))"
-				}
+					foreground: "hsl(var(--card-foreground))",
+				},
 			},
 			borderRadius: {
 				lg: "var(--radius)",
 				md: "calc(var(--radius) - 2px)",
-				sm: "calc(var(--radius) - 4px)"
+				sm: "calc(var(--radius) - 4px)",
 			},
 			fontFamily: {
-				sans: [...fontFamily.sans]
-			}
-		}
+				sans: [...fontFamily.sans],
+			},
+		},
 	},
-};
-
-export default config;
+	plugins: [forms, kayordPlugin],
+} satisfies Config;
