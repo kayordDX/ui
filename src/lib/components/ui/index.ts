1bcf670f2dd6458ee42789a1e68b92e28f0db3b8
Jaco Kok <kokjaco2@gmail.com>
2023-10-08 16:12:55 +0200

    New ts config and variant exports.

--- src/lib/components/ui/index.ts
+++ src/lib/components/ui/index.ts
@@ -1,5 +1,5 @@
-export { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "$lib/components/ui/accordion/index.js";
-export { Alert, AlertDescription, AlertTitle } from "$lib/components/ui/alert/index.js";
+export { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "./accordion/index.js";
+export { Alert, AlertDescription, AlertTitle } from "./alert/index.js";
 export {
 	AlertDialog,
 	AlertDialogAction,
@@ -12,14 +12,14 @@ export {
 	AlertDialogPortal,
 	AlertDialogTitle,
 	AlertDialogTrigger,
-} from "$lib/components/ui/alert-dialog/index.js";
-export { AspectRatio } from "$lib/components/ui/aspect-ratio/index.js";
-export { Avatar, AvatarFallback, AvatarImage } from "$lib/components/ui/avatar/index.js";
-export { Badge } from "$lib/components/ui/badge/index.js";
-export { Button, type ButtonEvents, type ButtonProps } from "$lib/components/ui/button/index.js";
-export { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "$lib/components/ui/card/index.js";
-export { Checkbox } from "$lib/components/ui/checkbox/index.js";
-export { Collapsible, CollapsibleContent, CollapsibleTrigger } from "$lib/components/ui/collapsible/index.js";
+} from "./alert-dialog/index.js";
+export { AspectRatio } from "./aspect-ratio/index.js";
+export { Avatar, AvatarFallback, AvatarImage } from "./avatar/index.js";
+export { Badge, type Variant as BadgeVariant, badgeVariants } from "./badge/index.js";
+export { Button, type ButtonEvents, type ButtonProps, buttonVariants } from "./button/index.js";
+export { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "./card/index.js";
+export { Checkbox } from "./checkbox/index.js";
+export { Collapsible, CollapsibleContent, CollapsibleTrigger } from "./collapsible/index.js";
 export {
 	ContextMenu,
 	ContextMenuCheckboxItem,
@@ -35,8 +35,8 @@ export {
 	ContextMenuSubContent,
 	ContextMenuSubTrigger,
 	ContextMenuTrigger,
-} from "$lib/components/ui/context-menu/index.js";
-export { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogOverlay, DialogPortal, DialogTitle, DialogTrigger } from "$lib/components/ui/dialog/index.js";
+} from "./context-menu/index.js";
+export { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogOverlay, DialogPortal, DialogTitle, DialogTrigger } from "./dialog/index.js";
 export {
 	DropdownMenu,
 	DropdownMenuCheckboxItem,
@@ -52,11 +52,11 @@ export {
 	DropdownMenuSubContent,
 	DropdownMenuSubTrigger,
 	DropdownMenuTrigger,
-} from "$lib/components/ui/dropdown-menu/index.js";
-export { Form, FormField, FormItem, FormLabel, FormDescription, FormValidation } from "$lib/components/ui/form/index.js";
-export { HoverCard, HoverCardContent, HoverCardTrigger } from "$lib/components/ui/hover-card/index.js";
-export { Input, type InputEvents } from "$lib/components/ui/input/index.js";
-export { Label } from "$lib/components/ui/label/index.js";
+} from "./dropdown-menu/index.js";
+export { Form, FormField, FormItem, FormLabel, FormDescription, FormValidation } from "./form/index.js";
+export { HoverCard, HoverCardContent, HoverCardTrigger } from "./hover-card/index.js";
+export { Input, type InputEvents } from "./input/index.js";
+export { Label } from "./label/index.js";
 export {
 	Menubar,
 	MenubarCheckboxItem,
@@ -73,31 +73,18 @@ export {
 	MenubarSubContent,
 	MenubarSubTrigger,
 	MenubarTrigger,
-} from "$lib/components/ui/menubar/index.js";
-export { Popover, PopoverContent, PopoverTrigger } from "$lib/components/ui/popover/index.js";
-export { Progress } from "$lib/components/ui/progress/index.js";
-export { RadioGroup, RadioGroupInput, RadioGroupItem } from "$lib/components/ui/radio-group/index.js";
-export { Select, SelectContent, SelectGroup, SelectInput, SelectItem, SelectLabel, SelectSeparator, SelectTrigger, SelectValue } from "$lib/components/ui/select/index.js";
-export { Separator } from "$lib/components/ui/separator/index.js";
-export {
-	Sheet,
-	SheetClose,
-	SheetContent,
-	SheetDescription,
-	SheetFooter,
-	SheetHeader,
-	SheetOverlay,
-	SheetPortal,
-	SheetTitle,
-	SheetTrigger,
-} from "$lib/components/ui/sheet/index.js";
-export { Skeleton } from "$lib/components/ui/skeleton/index.js";
-export { Slider } from "$lib/components/ui/slider/index.js";
-export { Switch } from "$lib/components/ui/switch/index.js";
-export { Table, TableBody, TableCaption, TableCell, TableFooter, TableHead, TableHeader, TableRow } from "$lib/components/ui/table/index.js";
-export { Tabs, TabsContent, TabsList, TabsTrigger } from "$lib/components/ui/tabs/index.js";
-export { Textarea, type TextareaEvents } from "$lib/components/ui/textarea/index.js";
-export { Toggle } from "$lib/components/ui/toggle/index.js";
-export { Tooltip } from "$lib/components/ui/tooltip/index.js";
-
-export * as ButtonAll from "./button/index.js";
+} from "./menubar/index.js";
+export { Popover, PopoverContent, PopoverTrigger } from "./popover/index.js";
+export { Progress } from "./progress/index.js";
+export { RadioGroup, RadioGroupInput, RadioGroupItem } from "./radio-group/index.js";
+export { Select, SelectContent, SelectGroup, SelectInput, SelectItem, SelectLabel, SelectSeparator, SelectTrigger, SelectValue } from "./select/index.js";
+export { Separator } from "./separator/index.js";
+export { Sheet, SheetClose, SheetContent, SheetDescription, SheetFooter, SheetHeader, SheetOverlay, SheetPortal, SheetTitle, SheetTrigger } from "./sheet/index.js";
+export { Skeleton } from "./skeleton/index.js";
+export { Slider } from "./slider/index.js";
+export { Switch } from "./switch/index.js";
+export { Table, TableBody, TableCaption, TableCell, TableFooter, TableHead, TableHeader, TableRow } from "./table/index.js";
+export { Tabs, TabsContent, TabsList, TabsTrigger } from "./tabs/index.js";
+export { Textarea, type TextareaEvents } from "./textarea/index.js";
+export { Toggle } from "./toggle/index.js";
+export { Tooltip } from "./tooltip/index.js";
