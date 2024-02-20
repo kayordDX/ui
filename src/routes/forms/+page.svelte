04be75b1b6b5dc93e8be52af428119112a0a8a92
Jaco Kok <kokjaco2@gmail.com>
2024-02-20 08:52:36 +0200

    New forms and packages

--- src/routes/forms/+page.svelte
+++ src/routes/forms/+page.svelte
@@ -1,17 +1,22 @@
 <script lang="ts">
-	import { Card, Form } from "$lib";
-	// import * as Form from "$lib/components/ui/form";
+	import { Card, Form, Input } from "$lib";
 
 	import { z } from "zod";
-	import type { SuperValidated } from "sveltekit-superforms";
-
-	export let form: SuperValidated<FormSchema>;
+	import { type SuperValidated, superForm, type Infer } from "sveltekit-superforms";
+	import { zodClient } from "sveltekit-superforms/adapters";
 
 	const formSchema = z.object({
 		username: z.string().min(2).max(50),
 	});
 
+	export let data: SuperValidated<Infer<FormSchema>>;
+	const form = superForm(data, {
+		validators: zodClient(formSchema),
+	});
+
 	type FormSchema = typeof formSchema;
+
+	const { form: formData, enhance } = form;
 </script>
 
 <Card.Root class="m-5">
@@ -20,17 +25,17 @@
 	</Card.Header>
 	<Card.Content>
 		<div class="flex space-x-2">
-			<Form.Root method="POST" {form} schema={formSchema} let:config>
-				<Form.Field {config} name="username">
-					<Form.Item>
+			<form method="POST" use:enhance>
+				<Form.Field {form} name="username">
+					<Form.Control let:attrs>
 						<Form.Label>Username</Form.Label>
-						<Form.Input />
+						<Input {...attrs} bind:value={$formData.username} />
 						<Form.Description>This is your public display name.</Form.Description>
-						<Form.Validation />
-					</Form.Item>
+						<Form.FieldErrors />
+					</Form.Control>
 				</Form.Field>
 				<Form.Button>Submit</Form.Button>
-			</Form.Root>
+			</form>
 		</div>
 	</Card.Content>
 </Card.Root>
