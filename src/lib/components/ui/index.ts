63d8132c397c64f22cc32bab0c181254dbd354fd
Jaco Kok <kokjaco2@gmail.com>
2024-02-27 15:05:56 +0200

    Latest shad components. Included nested Drawer Included Resizable Latest packages.

--- src/lib/components/ui/index.ts
+++ src/lib/components/ui/index.ts
@@ -25,6 +25,7 @@ export * as Popover from "./popover/index.js";
 export { Progress } from "./progress/index.js";
 export * as RadioGroup from "./radio-group/index.js";
 export * as RangeCalendar from "./range-calendar/index.js";
+export * as Resizable from "./resizable/index.js";
 export * as Select from "./select/index.js";
 export { Separator } from "./separator/index.js";
 export * as Sheet from "./sheet/index.js";
