d107acf25f61bb3a6d65ffeda5d63c7b0b19d4b8
Jaco Kok <kokjaco2@gmail.com>
2024-11-06 21:51:37 +0200

    Tailwind config tailwind animate added. Latest packages and updates.

--- src/lib/components/ui/sidebar/sidebar-input.svelte
+++ src/lib/components/ui/sidebar/sidebar-input.svelte
@@ -15,6 +15,6 @@
 	bind:ref
 	bind:value
 	data-sidebar="input"
-	class={cn("focus-visible:ring-sidebar-ring h-8 w-full bg-background shadow-none focus-visible:ring-2", className)}
+	class={cn("h-8 w-full bg-background shadow-none focus-visible:ring-2 focus-visible:ring-sidebar-ring", className)}
 	{...restProps}
 />
