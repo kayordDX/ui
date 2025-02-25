a2993151ca157d82d31e04f6bdd9f14eb9d567f3
Jaco Kok <kokjaco2@gmail.com>
2025-02-25 09:56:06 +0200

    Sidebar fixes with bash for now.

---
+++ tw.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+GREEN=$'\e[0;32m'
+RED=$'\e[0;31m'
+NC=$'\e[0m'
+
+process_directory() {
+  local dir="$1"
+
+  if [[ ! -d "$dir" ]]; then
+    echo "Error: Directory '$dir' not found."
+    return 1
+  fi
+
+  find "$dir" -name "*.svelte" -print0 | while IFS= read -r -d $'\0' item; do
+    sed -i -E 's/\[--([^]]*)\]/\(--\1\)/g' $item
+    echo "${GREEN}${NC} "$item
+  done
+
+  find "$dir" -mindepth 1 -type d -print0 | while IFS= read -r -d $'\0' subdir; do
+    process_directory "$subdir"
+  done
+}
+
+process_directory "./src/lib/components/ui/"
+echo "${GREEN} Tailwind 4 updated all! ${NC}"
+exit 0
