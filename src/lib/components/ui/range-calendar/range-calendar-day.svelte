eb4dd3e2925de09990cdbd6ef07505314672ef44
Jaco Kok <kokjaco2@gmail.com>
2025-05-22 07:37:14 +0200

    Fixed double slashes

--- src/lib/components/ui/range-calendar/range-calendar-day.svelte
+++ src/lib/components/ui/range-calendar/range-calendar-day.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { RangeCalendar as RangeCalendarPrimitive } from "bits-ui";
-	import { buttonVariants } from "$lib/components/ui//button/index.js";
+	import { buttonVariants } from "$lib/components/ui/button/index.js";
 	import { cn } from "$lib/utils.js";
 
 	let { ref = $bindable(null), class: className, ...restProps }: RangeCalendarPrimitive.DayProps = $props();
