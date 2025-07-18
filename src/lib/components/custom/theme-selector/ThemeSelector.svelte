b779ea0e8559fb06aadfa58d094849f487ee87f0
Jaco Kok <kokjaco2@gmail.com>
2025-07-18 13:10:17 +0200

    Same update for theme selector.

--- src/lib/components/custom/theme-selector/ThemeSelector.svelte
+++ src/lib/components/custom/theme-selector/ThemeSelector.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { Sun, Moon } from "@lucide/svelte";
+	import { SunIcon, MoonIcon } from "@lucide/svelte";
 	import { resetMode, setMode } from "mode-watcher";
 	import * as DropdownMenu from "$lib/components/ui/dropdown-menu/index.js";
 	import { buttonVariants } from "$lib/components/ui/button/index.js";
@@ -13,8 +13,8 @@
 
 <DropdownMenu.Root>
 	<DropdownMenu.Trigger class={buttonVariants({ variant, size: "icon" })}>
-		<Sun class="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
-		<Moon class="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+		<SunIcon class="scale-100 rotate-0 !transition-all dark:scale-0 dark:-rotate-90" />
+		<MoonIcon class="absolute scale-0 rotate-90 !transition-all dark:scale-100 dark:rotate-0" />
 		<span class="sr-only">Toggle theme</span>
 	</DropdownMenu.Trigger>
 	<DropdownMenu.Content align="end">
