000547d2ec1bc18a92f426c99a127d209e96f394
Jaco Kok <kokjaco2@gmail.com>
2025-10-10 12:07:39 +0200

    Fixed warning ssr render mismatch

--- src/lib/components/custom/data-table/VisibilitySelect.svelte
+++ src/lib/components/custom/data-table/VisibilitySelect.svelte
@@ -1,6 +1,6 @@
 <script lang="ts" generics="T">
 	import * as DropdownMenu from "$lib/components/ui/dropdown-menu";
-	import { Button } from "$lib/components/ui/button";
+	import { Button, buttonVariants } from "$lib/components/ui/button";
 	import { Settings2Icon } from "@lucide/svelte";
 	import type { Table } from "@tanstack/table-core";
 
@@ -12,10 +12,8 @@
 </script>
 
 <DropdownMenu.Root>
-	<DropdownMenu.Trigger>
-		<Button variant="outline" size="sm">
-			<Settings2Icon class="mr-2 h-4 w-4" /> View
-		</Button>
+	<DropdownMenu.Trigger class={buttonVariants({ variant: "outline", size: "sm" })}>
+		<Settings2Icon /> View
 	</DropdownMenu.Trigger>
 	<DropdownMenu.Content>
 		<DropdownMenu.Label>Toggle columns</DropdownMenu.Label>
