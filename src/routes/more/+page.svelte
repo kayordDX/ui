5f46bcdf4a8e3180f1f59b1a0667e26ad3b5a221
Jaco Kok <kokjaco2@gmail.com>
2026-02-12 14:59:05 +0200

    Included chip

---
+++ src/routes/more/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { Card, Chip } from "$lib";
+</script>
+
+<Card.Root class="m-5">
+	<Card.Header>
+		<Card.Title>More</Card.Title>
+		<Card.Description>https://more-shadcn.noair.fun/</Card.Description>
+	</Card.Header>
+	<Card.Content>
+		<div>
+			<div class="text-muted-foreground mb-2 text-xs">Chip</div>
+			<Chip.Root>Test</Chip.Root>
+			<Chip.Root removable>React</Chip.Root>
+		</div>
+	</Card.Content>
+</Card.Root>
