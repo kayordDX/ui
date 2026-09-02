90b5f14daf7fef401f2e5c2d347a8aac0d89e0dc
Jaco Kok <kokjaco2@gmail.com>
2026-09-02 11:00:45 +0200

    Also exporting ControlledState type.

--- src/lib/components/custom/data-table/index.ts
+++ src/lib/components/custom/data-table/index.ts
@@ -1,6 +1,6 @@
 export { default as DataTable } from "./DataTable.svelte";
 export { createShadTable, features } from "./shad-table.svelte";
-export type { ShadTableOptions } from "./shad-table.svelte";
+export type { ShadTableOptions, ControlledState } from "./shad-table.svelte";
 export { createColumnHelperFor, type DataTableFeatures } from "./features";
 export { useTableUrlSync } from "./table-url-sync.svelte";
 export type { TableUrlSyncOptions } from "./table-url-sync.svelte";
